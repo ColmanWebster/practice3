5987f9ff66fec176ecf191284b297f5cfed9f68f
Colman Webster <roguefiend@yahoo.com>
2020-09-07 21:36:44 -0700

    comment 2

--- scripts/practice3.js
+++ scripts/practice3.js
@@ -1,2 +1,6 @@
 
-// document.write('hello, kitty.');
+//document.getElementsByClassName('cont')[0].textContent = "hello, kitties.";
+
+$(document).ready(function(){
+    //$('div#cont1').text('hello, kitty.');
+});
